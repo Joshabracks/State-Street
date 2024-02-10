4ae667f7a72fe242a24c6392030441edb8b5939e
Joshua Bracks <joshabracks@gmail.com>
2024-02-09 21:05:34 -0800

    fix html parse issues

--- example/index.js
+++ example/index.js
@@ -9,6 +9,10 @@ const template = parseSST(`
         <span class="itallic">Message 1: {{message1}}</span>
         <div>Message 2: {{message2}}</div>
     </div>
+    <div>
+        <button onclick="whatIsIt()">this is a {{whatItIs}}</button>
+    </div>
+    <button onclick="increment()">{{total}}</button>
 </body>
 `);
 
@@ -17,10 +21,27 @@ const template = parseSST(`
 const data = {
     title: "State Street",
     message1: "... This is message 1!",
-    message2: "... This is message 2!"
+    message2: "... This is message 2!",
+    total: 0,
+    whatItIs: 'button'
 }
 
+function increment() {
+    stateStreet.data.total++;
+}
+
+const THING_LIST = [
+    'potato chips', 'pink blouse', 'protein shake', 'cat', 'football', 'elephant tusk', 'whiskers', 'button', 'trophy', 'one single grape', 'Robert Downey Jr'
+]
+function whatIsIt() {
+    const rando = Math.random();
+    const val = Math.floor( rando * THING_LIST.length);
+    const thing = THING_LIST[val];
+    stateStreet.data.whatItIs = thing;
+}
 
 window.onload = () => {
     window.stateStreet = new State(template, data)
+    window.increment = increment;
+    window.whatIsIt = whatIsIt;
 }
